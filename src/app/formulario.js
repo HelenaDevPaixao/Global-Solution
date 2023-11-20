88e2f0805dc802a7a001ba5878bdbb958867e28e
Helena <helena.lopespaixao@hotmail.com.br>
2023-11-20 20:35:52 -0300

    Enviando o Projeto

---
+++ src/app/formulario.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Formulário(){
+    return (<>
+    <h1>Página Formulário</h1>
+    <Link href="/page.js">
+            Voltar
+          </Link>
+    </>)
+}
